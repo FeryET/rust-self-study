2d763a1f5a50f11baea0afa931e04ec0140c08b4
Farhood Etaati <farhoodetaati@gmail.com>
2024-08-23 02:32:09 +0330

    feat: creates rust-cat project

--- projects/rust-cat/src/main.rs
+++ projects/rust-cat/src/main.rs
@@ -1,3 +1,23 @@
+use std::{
+    self, env, fs,
+    io::{stdout, Write},
+    path::Path,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut writer = std::io::BufWriter::new(stdout());
+    let args: Vec<String> = env::args().collect();
+    args[1..]
+        .iter()
+        .map(Path::new)
+        .map(|fpath| {
+            fs::read_to_string(fpath)
+                .expect(&format!("Cannot open path: {}", fpath.to_str().unwrap()))
+        })
+        .for_each(|out| {
+            if let Err(e) = writeln!(writer, "{}", out) {
+                eprintln!("Error writing to output: {}", e);
+            }
+        });
+    // writer.flush().expect_err("Error flushing output!");
 }
